10be5f637c06ea3f3df5a21873497bcca7659cec
H <43181787+Hzera@users.noreply.github.com>
2021-12-18 01:16:11 -0300

    Add files via upload

---
+++ PC_Clean.ps1
@@ -0,0 +1,32 @@
+﻿
+	$objShell = New-Object -ComObject Shell.Application
+	$objFolder = $objShell.Namespace(0xA)
+	$WinTemp = "c:\Windows\Temp\*"
+	
+#1# Recycle Bin #
+	write-Host "Cleaning the Recycle Bin" -ForegroundColor Cyan 
+	$objFolder.items() | %{ remove-item $_.path -Recurse -Confirm:$false}
+	
+#2# Remove Temporary
+	write-Host "Removing the Temporary Files" -ForegroundColor Green
+    Set-Location “C:\Windows\Temp”
+	Remove-Item * -Recurse -Force -ErrorAction SilentlyContinue
+
+    Set-Location “C:\Windows\Prefetch”
+    Remove-Item * -Recurse -Force -ErrorAction SilentlyContinue
+
+    Set-Location “C:\Documents and Settings”
+    Remove-Item “.\*\Local Settings\temp\*” -Recurse -Force -ErrorAction SilentlyContinue
+
+    Set-Location “C:\Users”
+    Remove-Item “.\*\Appdata\Local\Temp\*” -Recurse -Force -ErrorAction SilentlyContinue
+	
+#3# Running DCU
+	write-Host "Running the Disk Clean Up" -ForegroundColor Cyan
+	cleanmgr /sagerun:1 | out-Null 
+	
+	$([char]7)
+	Sleep 3
+	write-Host "We finished, thank's for using! :D" -ForegroundColor Magenta
+	Sleep 3
+##### End  ##### 
